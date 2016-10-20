6aa7b4c61fe1b8fe9b51789f0cceef41b4eed79e
Geoff Tipton <geoff.tipton@kroger.com>
2016-10-20 09:20:08 -0400

    initial

---
+++ save_etcd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]
+  then
+      echo "Wrong number of arguments supplied."
+      echo "Correct syntax: save_etcd.sh [etcd_url] [key] [file output]."
+      exit 1
+fi
+
+ETCD_URL=$1
+echo "ETCD_URL $ETCD_URL" 
+KEY=$2
+FILE_OUT=$3
+
+LINES="$(curl $ETCD_URL/v2/keys/$KEY | python -m json.tool | grep value | sed 's/ //g' | cut -d ':' -f2-13 | sed 's/\\//g')"
+for i in $LINES; do
+    $(echo $i >> $FILE_OUT)
+done
+
+
+
